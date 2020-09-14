88f190f15f7251e095c37bfc5691e0cd31b81972
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2020-09-14 22:37:58 +0530

    handle dynamic porst assignment by heroku

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.get('/getFiles', function (req, res) {
 })
 
 // start the server with specified port
-var server = app.listen(8081, function () {
+var server = app.listen(process.env.PORT || 8081, function () {
    var host = server.address().address
    var port = server.address().port
 
